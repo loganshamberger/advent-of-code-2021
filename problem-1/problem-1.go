874b8be9214677a23a47bc4a12bd298a912780ba
Logan Shamberger <loganshamberger@gmail.com>
2021-12-12 19:16:17 -0500

    Solution to part 1 of problem 1

--- problem-1/problem-1.go
+++ problem-1/problem-1.go
@@ -18,10 +18,9 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	var currentNum int
-	var increases int
-	currentNum = 0
-	increases = 0
+	var result []int
+	currentNum := 0
+	increases := 0
 	for scanner.Scan() {
 
 		readIn, err := strconv.Atoi(scanner.Text())
@@ -33,11 +32,34 @@ func main() {
 			increases++
 		}
 		currentNum=readIn
+		result= append(result, readIn)
 	}
 
 	if err := scanner.Err(); err != nil {
         log.Fatal(err)
     }
 	fmt.Println(increases-1)
+	fmt.Println(numberOfIncreases(slidingWindowSum(result)))
+}
+
+// Function to generate window sum
+func slidingWindowSum(arr []int) []int {
+	var slide []int
+
+	for i := 0; i< len(arr)-2; i++ {
+		element := arr[i]+arr[i+1]+arr[i+2]
+		slide= append(slide, element)
+	}
+	return slide
+}
+
+func numberOfIncreases(arr []int) int {
+	increase := 0
+	for i := 0; i<len(arr)-1; i++ {
+		if(arr[i]<arr[i+1]){
+			increase++
+		}
+	}
+	return increase
 }
 
