ba4e700a8e96d0c320943697af64389e58246543
Logan Shamberger <loganshamberger@gmail.com>
2021-12-22 18:35:09 -0500

    Not as neat and tested but probelm 6 solved

--- problem-6/laternfish-simulator.go
+++ problem-6/laternfish-simulator.go
@@ -1,23 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-func LanternfishSimulation(inputList []int, days int) (res []int) {
+func LanternfishSimulation(inputList []int, days int) (res int) {
 	var result []int
 	result = inputList
-	for i:=0; i<days; i++{
-		newFish := 0;
-		for j := 0; j<len(result); j++{
-			if(result[j]>0){
-				result[j]--
-			}else{
+	population := len(result)
+	for i := 0; i < days; i++ {
+		newFish := 0
+		for j := 0; j < len(result); j++ {
+			if result[j] == 0 {
 				newFish++
-				result[j]=6
+				population++
+				result[j] = 6
+			} else {
+				result[j]--
 			}
 		}
-		
+		for k := 0; k < newFish; k++ {
+			result = append(result, 8)
+		}
 		fmt.Printf("There are %d fish on day %d \n", len(result), i+1)
 	}
-	return result
+	return population
 }
 
+func LanternfishSimulationOptimized(inputList string, days int) (res int) {
+	fishData := [9]int{}
+	population := 0
+	//Perform initialSort
+	for i := 0; i < len(fishData); i++ {
+		fishData[i] = strings.Count(inputList, strconv.Itoa(i))
+		population += fishData[i]
+	}
+
+	fmt.Println(population)
+	var newFish int
+
+	for j := 0; j < days; j++ {
+		newFish = fishData[0]
+		population += newFish
+		fishData[0] = fishData[1]
+		fishData[1] = fishData[2]
+		fishData[2] = fishData[3]
+		fishData[3] = fishData[4]
+		fishData[4] = fishData[5]
+		fishData[5] = fishData[6]
+		fishData[6] = newFish + fishData[7]
+		fishData[7] = fishData[8]
+		fishData[8] = newFish
+		fmt.Printf("Day %d Fish Data %v Population %d \n", j+1, fishData, population)
+	}
+
+	return 0
+
+}
--- problem-6/laternfish-simulator_test.go
+++ problem-6/laternfish-simulator_test.go
@@ -8,20 +8,20 @@ import (
 type simulationTest struct {
 	inputList []int
 	days int
-	expectedFinalState []int
+	expectedPopulation int
 }
 
 var simulationTests = []simulationTest {
-	simulationTest{[]int{3,4,3,1,2},1,[]int{2,3,2,0,1}},
-	simulationTest{[]int{3,4,3,1,2},2,[]int{1,2,1,6,0,8}},
+	simulationTest{[]int{3,4,3,1,2},1, 5},
+	simulationTest{[]int{3,4,3,1,2},2, 6},
+	simulationTest{[]int{3,4,3,1,2},18,26},
 }
 
 func TestSimulation(t *testing.T) {
 	
 	for _, test := range simulationTests {
-		if output := LanternfishSimulation(test.inputList, test.days); !reflect.DeepEqual(output, test.expectedFinalState) {
-			t.Log( reflect.DeepEqual(output, test.expectedFinalState))
-			t.Errorf("Output %v not equal to expected %v", output, test.expectedFinalState)
+		if output := LanternfishSimulation(test.inputList, test.days); !reflect.DeepEqual(output, test.expectedPopulation) {
+			t.Errorf("Output %v not equal to expected %v", output, test.expectedPopulation)
 		}
 	}
 }
